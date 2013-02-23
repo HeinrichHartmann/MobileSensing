26dce6ee655e9c218593c832a3aaf36fcc93d25a
Heinrich <derhein@gmail.com>
2013-02-23 16:07:33 +0100

    Added some docs for AccSensorService

--- SensorApp/src/eu/livegov/mobilesensing/sensors/AccelerometerSensorService.java
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/AccelerometerSensorService.java
@@ -5,40 +5,48 @@ import java.util.List;
 
 public class AccelerometerSensorService extends SensorService {
 
-	public class AccelerometerMetadata implements Metadata {
+	public class AccelerometerMetadata implements Metadata {@Override
+	public String getName() {
+			// TODO Auto-generated method stub
+			return null;
+	}
 	}
 	
 	public class AccelerometerSensorValue implements SensorValue {
-		
+	@Override
+		public long getTimestamp() {
+			// TODO Auto-generated method stub
+			return 0;
+		}	
 	}
 	
 	@Override
 	public Metadata getMetadata() {
-		// TODO Auto-generated method stub
+		// Returns Object containg basic infromation about the sensor
 		return null;
 	}
 
 	@Override
 	public SensorValue getLastValue() {
-		// TODO Auto-generated method stub
+		// Returns last Recorded sensor value
 		return null;
 	}
 
 	@Override
 	public List<SensorValue> pullData() {
-		// TODO Auto-generated method stub
+		// Returns all SensorValues
+		// Clears the internal data queue
 		return null;
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		// Start service		
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
+		// Stop Service
 		
 	}
 
