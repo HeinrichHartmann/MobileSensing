a70aa1d558556881414c9286a7ac5d7a00bb0937
Richard Schütz <rschuetz@uni-koblenz.de>
2013-02-23 14:07:12 +0100

    Added AccelerometerSensorService example

---
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/AccelerometerSensorService.java
@@ -0,0 +1,34 @@
+package eu.livegov.mobilesensing.sensors;
+
+import java.util.List;
+
+
+public class AccelerometerSensorService extends SensorService {
+
+	public class AccelerometerMetadata implements Metadata {
+		
+	}
+	
+	public class AccelerometerSensorValue implements SensorValue {
+		
+	}
+	
+	@Override
+	public Metadata getMetadata() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SensorValue getLastValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SensorValue> pullData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
