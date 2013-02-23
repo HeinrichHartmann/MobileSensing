673c4662e39b0e700e5d6770956f2b52aee51458
Heinrich <derhein@gmail.com>
2013-02-23 16:30:12 +0100

    fixe

--- SensorApp/src/eu/livegov/mobilesensing/sensors/SensorService.java
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/SensorService.java
@@ -13,7 +13,7 @@ public abstract class SensorService extends Service {
 	 */
 	public abstract Metadata getMetadata();
 	public abstract SensorValue getLastValue();
-	public abstract putSensorValue(SensorValue value);
+	public abstract void putSensorValue(SensorValue value);
 	
 	public abstract List<SensorValue> pullData();
 	
