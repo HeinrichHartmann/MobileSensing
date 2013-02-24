f2d0b249b472fd5f5e8ee2bddb56adcf8fdb1688
joriks13 <jorik@schuetteler.net>
2013-02-24 15:13:01 +0100

    GyroValue added

--- SensorApp/src/eu/livegov/mobilesensing/sensors/gyroscope/GyroscopeSensorValue.java
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/gyroscope/GyroscopeSensorValue.java
@@ -2,12 +2,37 @@ package eu.livegov.mobilesensing.sensors.gyroscope;
 
 import eu.livegov.mobilesensing.sensors.SensorValue;
 
-public class GyroscopeSensorValue implements SensorValue{
+/**
+ * Container Object for Accelerometer Sensor Values
+ *
+ */
 
+public class GyroscopeSensorValue implements SensorValue{
+	public long timestamp;
+	public float x;
+	public float y;
+	public float z;
+	
+	// Constructor
+	public GyroscopeSensorValue(long timestamp, float x, float y,
+			float z) {
+
+		this.timestamp = timestamp;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 	@Override
 	public long getTimestamp() {
-		// TODO Auto-generated method stub
-		return 0;
+		return timestamp;
+	}
+	
+	public String toString() {
+		return "Gyroscope Value at " + 
+				String.valueOf(timestamp) + ":" +
+				String.valueOf(x) + "," + 
+				String.valueOf(y) + "," + 
+				String.valueOf(z); 
 	}
 
 }
