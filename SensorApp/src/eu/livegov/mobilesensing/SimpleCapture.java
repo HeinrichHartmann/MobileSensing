6b117603400be9f44f291c575be5ce6a81d8b3cc
Richard Schütz <rschuetz@uni-koblenz.de>
2013-02-24 18:44:04 +0100

    added some logging

--- SensorApp/src/eu/livegov/mobilesensing/SimpleCapture.java
+++ SensorApp/src/eu/livegov/mobilesensing/SimpleCapture.java
@@ -5,33 +5,40 @@ import eu.livegov.mobilesensing.manager.SensorManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class SimpleCapture extends Activity {
 
+	private final static String LOG_TAG = "SimpleCaptureActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.simple_capture);
 
+		Log.i(LOG_TAG, "starting SensorManager");
 		Intent service = new Intent(this, SensorManager.class);
 		startService(service);
 
 	}
 
 	public void startRecording(View view) {
+		Log.i(LOG_TAG, "sending ACTION_START_RECORDING");
 		Intent service = new Intent(this, SensorManager.class);
 		service.setAction(SensorManager.ACTION_START_RECORDING);
 		startService(service);
 	}
 
 	public void stopRecording(View view) {
+		Log.i(LOG_TAG, "sending ACTION_STOP_RECORDING");
 		Intent service = new Intent(this, SensorManager.class);
 		service.setAction(SensorManager.ACTION_STOP_RECORDING);
 		startService(service);
 	}
 
 	public void storeData(View view) {
+		Log.i(LOG_TAG, "sending ACTION_WRITE_DATA");
 		Intent service = new Intent(this, SensorManager.class);
 		service.setAction(SensorManager.ACTION_WRITE_DATA);
 		startService(service);
