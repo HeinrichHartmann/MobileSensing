3223d477fecd92ddd6970bc7e17005050b77c48f
Heinrich <derhein@gmail.com>
2013-02-24 16:34:15 +0100

    Some fixes

--- SensorApp/src/eu/livegov/mobilesensing/manager/SensorDescription.java
+++ SensorApp/src/eu/livegov/mobilesensing/manager/SensorDescription.java
@@ -3,12 +3,45 @@ package eu.livegov.mobilesensing.manager;
 import eu.livegov.mobilesensing.sensors.SensorService;
 
 public class SensorDescription {
-	public String sensorName;
-	public Class<? extends SensorService> serviceClass;
-	public SensorService serviceObject;
+	private String sensorName;
+	private Class<? extends SensorService> serviceClass;
+	private SensorService serviceObject;
 	
 	public SensorDescription(Class<? extends SensorService> serviceClass) {
 		this.serviceClass = serviceClass;
 		this.sensorName = serviceClass.getSimpleName(); 
 	}
+	
+	public boolean isBound() {
+		return (serviceObject != null);		
+	}
+
+	public boolean isRunning() {
+		if (! isBound() ) return false;
+		return serviceObject.running;
+	}
+	
+	public boolean isRecording() {
+		if (!isRunning()) return false;
+		return serviceObject.recording;
+	}
+
+	///// SETTERS/GETTERS ////
+	public String getSensorName() {
+		return sensorName;
+	}
+	
+	public Class<? extends SensorService> getServiceClass() {
+		return serviceClass;
+	}
+
+	public SensorService getServiceObject() {
+		return serviceObject;
+	}
+
+	public void setServiceObject(SensorService serviceObject) {
+		this.serviceObject = serviceObject;
+	}
+		
+	
 }
