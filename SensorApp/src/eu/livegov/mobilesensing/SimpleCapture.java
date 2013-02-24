a377a42fcd592aa3e42e89ca6dc2c4238688ace5
Richard Schütz <rschuetz@uni-koblenz.de>
2013-02-24 18:40:15 +0100

    modified SimpleCapture to use SensorManager

--- SensorApp/src/eu/livegov/mobilesensing/SimpleCapture.java
+++ SensorApp/src/eu/livegov/mobilesensing/SimpleCapture.java
@@ -1,110 +1,40 @@
 package eu.livegov.mobilesensing;
 
-import java.util.Timer;
-
 import eu.livegov.mobilesensing.R;
-import eu.livegov.mobilesensing.R.id;
-import eu.livegov.mobilesensing.R.layout;
-import eu.livegov.mobilesensing.sensors.SensorValue;
-import eu.livegov.mobilesensing.sensors.accelerometer.AccelerometerSensorService;
-import eu.livegov.mobilesensing.sensors.gps.GpsSensorService;
+import eu.livegov.mobilesensing.manager.SensorManager;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
 
-public class SimpleCapture extends  Activity{
-	
-	TextView gps, accelerometer;
-	AccelerometerSensorService accelerometerService = new AccelerometerSensorService();
-	GpsSensorService gpsService = new GpsSensorService();
-	boolean recording;
-	
+public class SimpleCapture extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.simple_capture);
-		
-		
-		
-		accelerometer = (TextView) findViewById(R.id.textAccelerometer);
-		gps = (TextView) findViewById(R.id.textGPS);
-		
-		/**
-		 * Checkbox-Listener
-		 */
-		CheckBox repeatChkBx =
-			    ( CheckBox ) findViewById( R.id.checkBox1 );
-			repeatChkBx.setOnCheckedChangeListener(new OnCheckedChangeListener()
-			{
-			    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-			    {
-			        if ( isChecked )
-			        {
-			           // startRecording();
-			        }
 
-			    }
-			});
-		
-		
-		
-	}
-	public void startRecording(View view){
-		
-		recording=true;
-		
-		Context context = getApplicationContext();
-		Intent service1 = new Intent(context, AccelerometerSensorService.class);
-		Intent service2 = new Intent(context, GpsSensorService.class);
-		
-		context.startService(service1);
-		context.startService(service2);
-		
-		
-			
-		CountDownTimer timer = new CountDownTimer(1000 ,500){
-
-			@Override
-			public void onFinish() {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onTick(long arg0) {
-				// TODO Auto-generated method stub
-				
-				
-				accelerometer.setText(String.valueOf(accelerometerService.getLastValue()));
-				gps.setText(String.valueOf(gpsService.getLastValue()));
-			}
-			
-			
-		};
-	
-	
+		Intent service = new Intent(this, SensorManager.class);
+		startService(service);
 
 	}
-	public void stopRecording(View view){
-		recording=false;
-		accelerometer.setText(String.valueOf(0));
-		gps.setText(String.valueOf(0));
-		
-	}
-	
-	
-	
-	
-	
 
-	
+	public void startRecording(View view) {
+		Intent service = new Intent(this, SensorManager.class);
+		service.setAction(SensorManager.ACTION_START_RECORDING);
+		startService(service);
+	}
+
+	public void stopRecording(View view) {
+		Intent service = new Intent(this, SensorManager.class);
+		service.setAction(SensorManager.ACTION_STOP_RECORDING);
+		startService(service);
+	}
+
+	public void storeData(View view) {
+		Intent service = new Intent(this, SensorManager.class);
+		service.setAction(SensorManager.ACTION_WRITE_DATA);
+		startService(service);
+	}
 
 }
-
