a73a8bf1cc4aa16746914530712b60288005c1ef
joriks13 <jorik@schuetteler.net>
2013-02-24 15:04:18 +0100

    added blank gyro

---
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/gyroscope/GyroscopeSensorService.java
@@ -0,0 +1,35 @@
+package eu.livegov.mobilesensing.sensors.gyroscope;
+
+import java.util.List;
+
+import eu.livegov.mobilesensing.sensors.Metadata;
+import eu.livegov.mobilesensing.sensors.SensorService;
+import eu.livegov.mobilesensing.sensors.SensorValue;
+
+public class GyroscopeSensorService extends SensorService{
+
+	@Override
+	public Metadata getMetadata() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SensorValue getLastValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<? extends SensorValue> pullData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putSensorValue(SensorValue value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ SensorApp/src/eu/livegov/mobilesensing/sensors/gyroscope/GyroscopeSensorValue.java
@@ -0,0 +1,13 @@
+package eu.livegov.mobilesensing.sensors.gyroscope;
+
+import eu.livegov.mobilesensing.sensors.SensorValue;
+
+public class GyroscopeSensorValue implements SensorValue{
+
+	@Override
+	public long getTimestamp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
