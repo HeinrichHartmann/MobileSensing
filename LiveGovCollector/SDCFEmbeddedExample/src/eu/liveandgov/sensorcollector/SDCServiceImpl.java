09394d78ed63fb7d7ff91671f6a824c3e88e08cb
Christoph Schaefer <chrisschaefer@uni-koblenz.de>
2013-06-12 11:34:20 +0200

    rename liveandgoc to liveandgov

--- LiveGovCollector/SDCFEmbeddedExample/src/eu/liveandgov/sensorcollector/SDCServiceImpl.java
+++ LiveGovCollector/SDCFEmbeddedExample/src/eu/liveandgov/sensorcollector/SDCServiceImpl.java
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with the SDCFramework.  If not, see <http://www.gnu.org/licenses/>.
  */
-package eu.liveandgoc.sensorcollector;
+package eu.liveandgov.sensorcollector;
 
 import de.unikassel.android.sdcframework.app.AbstractSDCServiceImpl;
 import android.annotation.SuppressLint;
