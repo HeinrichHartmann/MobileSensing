5d6d889ccfff2f7ad12712e20c4ce15c6c9f72b5
Christoph Ehlen <christoph@ehlen.de>
2013-06-24 10:53:54 +0200

    Changed Debug data inspection

--- nodejs/staticServ/public/javascripts/api.js
+++ nodejs/staticServ/public/javascripts/api.js
@@ -1,24 +1,41 @@
 (function () {
   
-  var apiUrl = 'http://127.0.0.1:8080';
+  var apiUrl = 'http://mobile-sensing.west.uni-koblenz.de:8888';
 
   $(function () {
+    $('.from').datetimepicker();
+    $('.to').datetimepicker();
+
     // Get all UUIDS
     $.ajax({
       url: apiUrl + '/devices'
     }).done(function (data) {
       var uuid = $('.uuid');
       $.each(data, function (id, item) {
-        uuid.append('<option>' + item.uuid + '</option>');
+        uuid.append('<option value="' + item.uuid + '">' + item.uuid + ' ( ' + item.device + ' )' + '</option>');
       });
     });
 
-    $('.uuid').change(function (foo) {
+    var addTimeRange = function () {
+      var from = Date.parse($('.from').val());
+      var to = Date.parse($('.to').val());
+      return 'from=' + from + '&to=' + to;
+    };
+
+    var addLimit = function () {
+      var limit = $('.limit').val();
+      if(!limit || limit === 0) {
+        return '';
+      }
+      return 'limit=' + limit;
+    };
+
+    var getData = function (foo) {
       var uuid = $('.uuid').val();
       
       // Gps
       $.ajax({
-        url: apiUrl + '/' + uuid + '/gps?limit=10' 
+        url: apiUrl + '/' + uuid + '/gps?limit=10&' + addTimeRange()
       }).done(function (data) {
         var $gps = $('.gps');
         $.each(data, function (id, item) {
@@ -28,9 +45,10 @@
 
       // Acc
       $.ajax({
-        url: apiUrl + '/' + uuid + '/accelerometer'
+        url: apiUrl + '/' + uuid + '/accelerometer?' + addTimeRange() + '&' + addLimit()
       }).done(function (data) {
         var $acc = $('.acc');
+        $('.magCount').val(data.length);
         var x = [];
         var y = [];
         var z = [];
@@ -50,9 +68,10 @@
 
       // Mag
       $.ajax({
-        url: apiUrl + '/' + uuid + '/magneticfield'
+        url: apiUrl + '/' + uuid + '/magneticfield?' + addTimeRange() + '&' + addLimit()
       }).done(function (data) {
         var $mag = $('.mag');
+        $('.magCount').val(data.length);
         var x = [];
         var y = [];
         var z = [];
@@ -72,9 +91,10 @@
 
       // Gyro
       $.ajax({
-        url: apiUrl + '/' + uuid + '/gyroscope'
+        url: apiUrl + '/' + uuid + '/gyroscope?' + addTimeRange() + '&' + addLimit()
       }).done(function (data) {
         var $gyro = $('.gyro');
+        $('.gyroCount').val(data.length);
         var x = [];
         var y = [];
         var z = [];
@@ -92,7 +112,10 @@
         var plot = $.plot($gyro, plotData);
       });
 
-    });
+    };
+
+    $('.uuid').change(getData);
+    $('.go').click(getData);
   });
 
 })();
